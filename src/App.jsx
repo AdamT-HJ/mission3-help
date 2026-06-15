cd779f4374b773be10c0b6bafd28d4683839ba4e
AdamT <197536872+AdamT-HJ@users.noreply.github.com>
2026-06-16 10:54:58 +1200

    setup commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+
+// image import from assets example
+import animation from './assets/Home/animation.png'
+
+
+function App() {
+
+
+
+  
+
+  return (
+  <>
+    <h1>Lets start from here</h1>
+    
+ 
+  </>
+  );
+}
+
+export default App;
